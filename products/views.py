42b87f94c6e3fc67a8eb5df90bf78be0cac69aee
paulovc26 <paulovcsiqueira@gmail.com>
2024-03-08 08:55:51 -0300

    subindo

---
+++ products/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def list_products(request):
+    products = Product.objects.all()
+    return render(request, 'list_products.html', {'products': products})
